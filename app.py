b6eeb5e4832b95c613e64a06155378adaebd0af2
cursopythonocean <flaviomalheiros@gmail.com>
2021-11-20 22:14:40 +0000

    Fim

--- app.py
+++ app.py
@@ -8,37 +8,39 @@ import io
 import sys
 ##############Ocean###########
 import sqlite3
-from flask import Flask
+from flask import Flask, g, render_template, request
 ######Configure_OCEAN#########
-DATABASE = "./flaskr.db"
+DATABASE = "./flasker.db"
 SECRET_KEY = "pudim"
 USERNAME = "admin"
 PASSWORD = "admin"
 ######Aplicação OCEAN#########
 app = Flask(__name__)
 app.config.from_object(__name__)
+#######Conectando ao Banco de dados######
+def connect_db():
+    return sqlite3.connect(DATABASE)
+###Abrindo conexão#####
+@app.before_request
+def before():
+    g.db = connect_db()
+###Fechando conexão####
+@app.teardown_request
+def after(exception):
+    g.db.close()
+#####Criando rota para aplição#######
 @app.route('/')
 def index():
     #leitura=open("./arqText.txt", "r")
     #print (leitura.read())
     #leitura.close()
-    return ("Hello word")
-def connect_db():
-    return sqlite3.connect(DATABASE)
-##############Google###########
-def _upload():
-
-    _upload_widget = fileupload.FileUploadWidget()
-
-    def _cb(change):
-        global file_contents
-        decoded = io.StringIO(change['owner'].data.decode('utf-8'))
-        filename = change['owner'].filename
-        print('Uploaded `{}` ({:.2f} kB)'.format(
-            filename, len(decoded.read()) / 2 **10))
-        file_contents = decoded.getvalue()
-
-    _upload_widget.observe(_cb, names='data')
-    display(_upload_widget)
-
-_upload()
+    sql = 'SELECT titulo, texto from entradas order by id desc'
+    cur = g.db.execute(sql)
+    entradas = [dict(titulo=titulo, texto=texto) for titulo, texto in cur.fetchall()]
+    return render_template("index.html", entradas=entradas)
+@app.route('/inserir', methods=['POST'])
+def inserir():
+    sql = 'INSERT INTO entradas (titulo, texto) values(?,?)'
+    g.db.execute(sql, [request.form['titulo'], request.form['texto']])
+    g.db.commit()
+    return render_template('index.html')
